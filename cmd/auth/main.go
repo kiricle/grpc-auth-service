b74abf5488fb72c7770da485d4384f8a5406365d
Kolotylo Kyrylo <kolot102@gmail.com>
2025-02-15 19:34:15 +0200

    update module name

---
+++ cmd/auth/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	config "grpc-auth-service/internal"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting auth service", slog.String("env", cfg.Env))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,7 @@
+package auth
+
+// import authv1 "github.com/kiricle/protos/gen/go/auth"
+
+type serverAPI struct {
+	
+}
